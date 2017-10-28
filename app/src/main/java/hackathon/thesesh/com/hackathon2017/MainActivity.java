2539ba3fcd681368bbdf2a959b2ade6838ad395a
Sam <samuel.dacosta@student.manchester.ac.uk>
2017-10-28 15:54:28 +0100

    Update code styles

--- app/src/main/java/hackathon/thesesh/com/hackathon2017/MainActivity.java
+++ app/src/main/java/hackathon/thesesh/com/hackathon2017/MainActivity.java
@@ -2,14 +2,23 @@ package hackathon.thesesh.com.hackathon2017;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button createButton = findViewById(R.id.create_complaint_button);
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                // start new
+            }
+        });
     }
 }
