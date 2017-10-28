98be1a0d9ec045f1c29a8547da2b0c69fdfa9716
Unknown <jack.medley-hill@student.manchester.ac.uk>
2017-10-28 23:53:57 +0100

    Minor refactor

--- app/src/main/java/hackathon/thesesh/com/hackathon2017/IntroActivity.java
+++ app/src/main/java/hackathon/thesesh/com/hackathon2017/IntroActivity.java
@@ -4,16 +4,53 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class IntroActivity extends AppCompatActivity {
+import hackathon.thesesh.com.hackathon2017.engine.OmbudsmanApplication;
+import hackathon.thesesh.com.hackathon2017.engine.model.FormData;
+
+public class IntroActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText nameTextData;
+    private EditText emailTextData;
+    private EditText complaintTextData;
+    private Button goButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        setupViews();
+        goButton.setOnClickListener(this);
     }
 
     public static Intent createIntent(Context context) {
         return new Intent(context, IntroActivity.class);
+    }
+
+    private void setupViews() {
+        nameTextData = findViewById(R.id.nameText);
+        emailTextData = findViewById(R.id.emailText);
+        complaintTextData = findViewById(R.id.complaintText);
+        goButton = findViewById((R.id.go_button));
+    }
+
+    @Override
+    public void onClick(View view) {
+        FormData formData = ((OmbudsmanApplication)getApplication()).getFormData();
+        if (editTextPopulated(nameTextData) && editTextPopulated(emailTextData) && editTextPopulated(complaintTextData)) {
+            formData.setName(nameTextData.getText().toString());
+            formData.setEmail(emailTextData.getText().toString());
+            formData.setComplaint(complaintTextData.getText().toString());
+        }
+    }
+
+    private boolean editTextPopulated(EditText textData) {
+        if (textData.getText().toString().trim().length() == 0) {
+            return false;
+        }
+        return true;
     }
 }
